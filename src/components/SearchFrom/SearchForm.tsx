8631923fedb24cfddc0118286fa2d2cc03118f27
Kirill Medvedev <repkomail@gmail.com>
2024-03-06 20:16:03 +0200

    feat: add state to input

--- src/components/SearchFrom/SearchForm.tsx
+++ src/components/SearchFrom/SearchForm.tsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import "./styles.css";
 
 export function SearchForm() {
+  const [value, setValue] = useState('')
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setValue(e.target.value)
+  }
+
   return (
     <div className="searchForm">
       <form>
-        <input type="text" value="Terry" />
+        <input
+          onChange={handleChange}
+          type="text"
+          value={value} />
       </form>
     </div>
   );
